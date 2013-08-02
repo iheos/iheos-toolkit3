021ee0600a43c04144af7a2a920d934d1de45057
Bill Majurski <bmajur@gmail.com>
2013-08-01 23:36:17 -0400

    Add structure for Registry Actor Register Transaction sim.

--- registrySim/src/main/java/gov/nist/hit/ds/registrySim/actor/DocumentRegistryActorSim.java
+++ registrySim/src/main/java/gov/nist/hit/ds/registrySim/actor/DocumentRegistryActorSim.java
@@ -1,7 +1,6 @@
 package gov.nist.hit.ds.registrySim.actor;
 
 import gov.nist.hit.ds.errorRecording.ErrorContext;
-import gov.nist.hit.ds.initialization.Installation;
 import gov.nist.hit.ds.registrySim.datatypes.RegistrySimTransactionType;
 import gov.nist.hit.ds.simSupport.client.NoSimException;
 import gov.nist.hit.ds.simSupport.datatypes.SimEndPoint;
@@ -12,26 +11,34 @@ import gov.nist.hit.ds.simSupport.sim.SimDb;
 import gov.nist.hit.ds.soapSupport.core.FaultCodes;
 import gov.nist.hit.ds.soapSupport.exceptions.SoapFaultException;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
 
+/**
+ * This is the Document Registry actor simulator.  It must be configured in 
+ * the SimChain before the relevant transaction SimComponent
+ * @author bmajur
+ *
+ */
 public class DocumentRegistryActorSim extends SimComponentBase {
 	SimEndPoint simEndPoint;
-
+	SimDb db;
 	static Logger logger = Logger.getLogger(DocumentRegistryActorSim.class);
-	
+
 	@Inject
 	public void setSimEndPoint(SimEndPoint simEndPoint) {
 		this.simEndPoint = simEndPoint;
 	}
+	
+	public SimDb getSimDb() {
+		return db;
+	}
 
 	@Override
 	public void run(MessageValidatorEngine mve) throws SoapFaultException {
-		RegistrySimTransactionType transType = RegistrySimTransactionType.valueOf(simEndPoint.getTransaction());
+		er.challenge("Document Registry Sim running");
 		
-		SimDb db;
 		try {
 			db = new SimDb(simEndPoint.getSimId(), simEndPoint.getActor(), simEndPoint.getTransaction());
 		} catch (IOException e) {
@@ -44,17 +51,6 @@ public class DocumentRegistryActorSim extends SimComponentBase {
 					er,
 					FaultCodes.Sender,
 					new ErrorContext("Simulator <" + simEndPoint.getSimId() + "> does not exist"));
-		}
-
-		db.getTransInstances("foo");
-		
-		if (transType.equals(RegistrySimTransactionType.rb)) {
-			
-		} else {
-			throw new SoapFaultException(
-					er,
-					FaultCodes.EndpointUnavailable,
-					new ErrorContext("Transaction type <" + transType +"> not supported"));
 		}
 	}
 }
