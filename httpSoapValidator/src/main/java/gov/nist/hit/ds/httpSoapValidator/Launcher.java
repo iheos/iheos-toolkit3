851e924eed610fecb78ae3c55f7e14e4b9dfb27f
Bill Majurski <bmajur@gmail.com>
2013-08-01 23:35:47 -0400

    Add Launcher which takes a SimChain config file, loads it, and runs it.

---
+++ httpSoapValidator/src/main/java/gov/nist/hit/ds/httpSoapValidator/Launcher.java
@@ -0,0 +1,36 @@
+package gov.nist.hit.ds.httpSoapValidator;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
+import gov.nist.hit.ds.http.parser.HttpEnvironment;
+import gov.nist.hit.ds.httpSoapValidator.testSupport.HttpServletResponseMock;
+import gov.nist.hit.ds.simSupport.engine.SimChain;
+import gov.nist.hit.ds.simSupport.engine.SimChainLoader;
+import gov.nist.hit.ds.simSupport.engine.SimEngine;
+import gov.nist.hit.ds.simSupport.engine.SimEngineSubscriptionException;
+import gov.nist.hit.ds.soapSupport.core.SoapEnvironment;
+
+public class Launcher {
+
+	public Launcher launch(String chainDef) throws IOException, ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException, SimEngineSubscriptionException {
+		SimChain simChain = new SimChainLoader(chainDef).load();
+		
+		simChain.setBase(new SoapEnvironment(new HttpEnvironment().setResponse(new HttpServletResponseMock())));
+		
+		assertTrue(simChain.getSteps().size() > 0);
+
+		SimEngine engine = new SimEngine(simChain);
+		
+		System.out.println(engine.getDescription(simChain));
+
+		engine.run();
+
+		System.out.println(simChain.getLog());
+		
+		return this;
+
+	}
+}
