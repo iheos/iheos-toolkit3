b511fc96508d538fa4b18fc51aa7f415358b9d3d
Bill Majurski <bmajur@gmail.com>
2013-08-15 23:11:08 -0400

    Toolkit v3 now launches from within Eclipse.  Currently the UI content is a simple Hello World app.

--- actorSimFactory/src/main/java/gov/nist/hit/ds/actorSimFactory/servlet/SimServlet.java
+++ actorSimFactory/src/main/java/gov/nist/hit/ds/actorSimFactory/servlet/SimServlet.java
@@ -1,6 +1,6 @@
-package gov.nist.hit.ds.actorSim.servlet;
+package gov.nist.hit.ds.actorSimFactory.servlet;
 
-import gov.nist.hit.ds.actorSim.factory.ActorSimFactory;
+import gov.nist.hit.ds.actorSimFactory.factory.ActorSimFactory;
 import gov.nist.hit.ds.actorTransaction.ActorType;
 import gov.nist.hit.ds.errorRecording.ErrorContext;
 import gov.nist.hit.ds.http.environment.Event;
--- registrySim/src/test/java/gov/nist/hit/ds/registrySim/RegistrySimTest.java
+++ registrySim/src/test/java/gov/nist/hit/ds/registrySim/RegistrySimTest.java
@@ -3,8 +3,8 @@ package gov.nist.hit.ds.registrySim;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-import gov.nist.hit.ds.actorSim.factory.ActorSimFactory;
-import gov.nist.hit.ds.actorSim.servlet.SimServlet;
+import gov.nist.hit.ds.actorSimFactory.factory.ActorSimFactory;
+import gov.nist.hit.ds.actorSimFactory.servlet.SimServlet;
 import gov.nist.hit.ds.actorTransaction.AsyncType;
 import gov.nist.hit.ds.actorTransaction.TlsType;
 import gov.nist.hit.ds.actorTransaction.TransactionType;
@@ -39,6 +39,10 @@ import org.junit.Test;
 public class RegistrySimTest {
 	SimServlet servlet;
 	
+	/**
+	 * Perform initializations that would normally be done the the toolkit during startup.
+	 * In production, the src/java versions would be used instead of the src/test versions.
+	 */
 	@Before
 	public void init() {
 		Installation.installation().setExternalCache(new File("src/test/resources/external_cache"));
@@ -47,6 +51,8 @@ public class RegistrySimTest {
 		Installation.installation().setWarHome(warHome);
 		ExtendedPropertyManager.load(warHome);
 		new ActorSimFactory().setConfiguredSimsFile(new File("src/test/resources/configuredActorSims.properties"));
+		
+		// Initialize servlet - includes loading simulator definitions
 		servlet = new SimServlet();
 		try {
 			servlet.initSimEnvironment(); 
@@ -82,26 +88,42 @@ public class RegistrySimTest {
 
 	@Test
 	public void registerTest() {
-		ByConstructorLogLoader logLoader = new ByConstructorLogLoader(new File("src/test/resources/register"));
-		logLoader.run(null);
-		HttpServletRequest request = null;
+		
+		
 		try {
-			request = logLoader.getServletRequest();
+			
+			// Create a new Registry simulator with non-TLS and sync inputs only
 			String simId = "123";
 			DocumentRegistryActorFactory fact = new DocumentRegistryActorFactory();
 			Simulator sim =
 					fact.buildNewSimulator(new SimId(simId), 
 							new TlsType[]  { TlsType.NOTLS }, 
 							new AsyncType[] { AsyncType.SYNC });
+			
+			// verify proper endpoint was created
 			String endpointString = sim.getEndpoint(TransactionType.REGISTER, TlsType.NOTLS, AsyncType.SYNC);
 			assertFalse(endpointString == null);
+			
+			// Build mock servlet environment that allows test inputs to be injected
 			SimEndpoint simEndpoint = new SimEndpointParser().parse(endpointString);
 			Endpoint endpoint = new Endpoint().setEndpoint(endpointString);
+			
 			HttpEnvironment httpEnv = new HttpEnvironment().setResponse(new HttpServletResponseMock());
 			SoapEnvironment soapEnv = new SoapEnvironment(httpEnv);
 			soapEnv.setEndpoint(endpoint);
 
+			
+			// load the Register transaction inputs
+			ByConstructorLogLoader logLoader = new ByConstructorLogLoader(new File("src/test/resources/register"));
+			logLoader.run(null);
+
+			// build HTTP request
+			HttpServletRequest request = null;
+			request = logLoader.getServletRequest();
+			
+			// initiate Register transaction through SimServlet
 			servlet.handleSimulatorInputTransaction(request, soapEnv, simEndpoint, endpoint);
+			
 			assertTrue(servlet.getFaultSent() == null);
 		} catch (HttpParseException e) {
 			e.printStackTrace();
