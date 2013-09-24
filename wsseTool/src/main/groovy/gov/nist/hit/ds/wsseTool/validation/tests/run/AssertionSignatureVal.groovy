779dcdc5365c2254083c9c25dd02f941f9bfcc31
agerardin <none@none>
2013-09-24 18:10:58 -0400

    fitted assertion signature val
    --HG--
    branch : antoine_wsse_update

--- wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AssertionSignatureVal.groovy
+++ wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AssertionSignatureVal.groovy
@@ -1,15 +1,14 @@
 package gov.nist.hit.ds.wsseTool.validation.tests.run
 
-import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder;
+import static org.junit.Assert.*
+import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder
 import gov.nist.hit.ds.wsseTool.validation.engine.annotations.Validation
 import gov.nist.hit.ds.wsseTool.validation.tests.BaseVal
 import gov.nist.hit.ds.wsseTool.validation.tests.CommonVal
 
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TestName
-import org.junit.runner.RunWith;
+import java.text.MessageFormat
+
+import org.junit.runner.RunWith
 
 @RunWith(ValRunnerWithOrder.class)
 public class AssertionSignatureVal extends BaseVal {
@@ -17,18 +16,20 @@ public class AssertionSignatureVal extends BaseVal {
 	@Validation(id="1036", rtm=["59"])
 	public void uniqueId(){
 		if(! header.map.a_signature.@Id.isEmpty())
-			assert CommonVal.uniqueId(header.map.a_signature.@Id)
+			assertTrue(CommonVal.uniqueId(header.map.a_signature.@Id))
 	}
 	
 	@Validation(id="1038", rtm=["141"])
 	public void signInfoUniqueId(){
 		if(! header.map.as_signedInfo.@Id.isEmpty())
-			assert CommonVal.uniqueId(header.map.a_signature.@Id)
+			assertTrue(CommonVal.uniqueId(header.map.a_signature.@Id))
 	}
 	
 	@Validation(id="1043", rtm=["147"])
 	public void referenceUriIsAssertionId(){
 		String id = header.map.as_signedInfo.'*'.findAll { it.name() == "Reference" }.@URI.text()
-		assert id.stripIndent(1) == header.map.assertion.@ID.text(); //we remove the starting # in the ref
+		String referenceURI = id.stripIndent(1) //we remove the starting # in the ref
+		String assertionId = header.map.assertion.@ID.text();
+		assertEquals(MessageFormat.format("reference URI should match assertionID : {1} but was : {0}", referenceURI, assertionId), referenceURI , assertionId); 
 	}
 }
