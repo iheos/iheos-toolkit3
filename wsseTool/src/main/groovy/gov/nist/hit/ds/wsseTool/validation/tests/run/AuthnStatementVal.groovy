4ff12c244e6ecc9a0f84dc89f0e08661dfdc0a0a
agerardin <none@none>
2013-09-25 16:52:52 -0400

    fitted authStatementVal
    --HG--
    branch : antoine_wsse_update

--- wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AuthnStatementVal.groovy
+++ wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AuthnStatementVal.groovy
@@ -1,22 +1,22 @@
 package gov.nist.hit.ds.wsseTool.validation.tests.run
 
 
-import gov.nist.hit.ds.wsseTool.api.config.ValConfig
+import static org.junit.Assert.*
+import gov.nist.hit.ds.wsseTool.api.config.*
 import gov.nist.hit.ds.wsseTool.time.TimeUtil
 import gov.nist.hit.ds.wsseTool.validation.data.AuthnContextClassRef
-import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder;
+import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder
+import gov.nist.hit.ds.wsseTool.validation.engine.annotations.Optional
 import gov.nist.hit.ds.wsseTool.validation.engine.annotations.Validation
 import gov.nist.hit.ds.wsseTool.validation.tests.BaseVal
 import groovy.util.slurpersupport.GPathResult
 
+import java.text.MessageFormat
+
 import org.apache.commons.validator.routines.DomainValidator
 import org.apache.commons.validator.routines.InetAddressValidator
 import org.joda.time.DateTime
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TestName
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith
 
 
 @RunWith(ValRunnerWithOrder.class)
@@ -31,81 +31,72 @@ public class AuthnStatementVal extends BaseVal {
 			if(children[0].name() != "SubjectLocality") log.error("authn statement first child must be a subject locality but was ", children[0])
 			if(children[1].name() != "AuthnContext") log.error("authn statement second child must be an authn context but was ", children[1])
 
-			assert children[0].name() == "SubjectLocality"
-			assert children[1].name() == "AuthnContext"
+			assertEquals("authn statement first child must be a subject locality", "SubjectLocality", children[0].name())
+			assertEquals( "authn statement second child must be an authn context", "AuthnContext", children[1].name())
 		}
 
 		else {
 			//otherwise we check only the authnContext
-			if(children[0].name() != "AuthnContext") log.error("authn statement first child must be an authn context but was ", children[0])
-			assert children[0].name() == "AuthnContext"
+			assertEquals("authn statement first child must be an authn context", "AuthnContext" , children[0].name() )
 		}
 	}
 
 	@Validation(id="1076", rtm=["82"])
 	public void authnInstant(){
 		String authnInstant = header.map.authnStatement.@AuthnInstant.text()
-		if( !TimeUtil.isDateInUTCFormat(authnInstant) )log.error("authnInstant not in UTC format : {}", authnInstant)
-
+		assertTrue("authnInstant not in UTC format", TimeUtil.isDateInUTCFormat(authnInstant))
 
 		DateTime ai = TimeUtil.parseDateString(authnInstant)
 		String creationDate = header.map.timestamp.Created[0].text().trim()
 		DateTime creationTime = TimeUtil.parseDateString(creationDate)
 
-		if(ai.isAfter(creationTime)){
-			log.error("saml2:AuthnStatement@authnInstant {} should be earlier than wsu:Timestamp/wsu:Created {}", ai ,creationTime)
-		}
-
-		assert(!ai.isAfter(creationTime))
+		assertTrue(MessageFormat.format("saml2:AuthnStatement@authnInstant {0} should be earlier than wsu:Timestamp/wsu:Created {1}", ai, creationTime), !ai.isAfter(creationTime))
 	}
 
-	@Validation(id="1077", rtm=["79"], category="optional", status=ValConfig.Status.not_implemented)
+	@Optional
+	@Validation(id="1077", rtm=["79"], status=ValConfig.Status.not_implemented)
 	public void SessionIndex(){
 		log.info("session index is optional")
 	}
 
-	@Validation(id="1078", rtm=["78"], category="optional")
+	@Optional
+	@Validation(id="1078", rtm=["78"])
 	public void SubjectLocality(){
-
-
 		GPathResult loc = header.map.authnStatement.children.findAll{ it.@Name == "SubjectLocality"}
 
 		if(loc.isEmpty()){
 			log.info("subject locality not present but is optional."); return
 		}
 
-		if(loc.@DNSName == null) log.error("DNSName should be present")
-		assert loc.@DNSName != null
+		assertTrue("DNSName should be present", loc.@DNSName != null)
 
-		if(loc.@Address == null) log.error("Address should be present")
-		assert loc.@Address != null
+		assertTrue("Address should be present", loc.@Address != null)
 
 		DomainValidator dnsVal = DomainValidator.getInstance()
 		boolean validDNSName = dnsVal.isValid(loc.@DNSName.text())
 
 		if(!validDNSName) log.error("invalid DNSName. Found : {} ",loc.@DNSName.text() )
-		assert validDNSName
+		assertTrue( MessageFormat.format("invalid DNSName. Found : {0} ",loc.@DNSName.text()),  validDNSName)
 
 		InetAddressValidator ipVal = InetAddressValidator.getInstance()
 		boolean validIP = dnsVal.isValid(loc.@Address.text())
 
-		if(! validIP) log.error("invalid InetAddress. Found : {} ", loc.@Address.text() )
-		assert validIP
+
+		assertTrue( MessageFormat.format("invalid InetAddress. Found : {0} ",loc.@Address.text()), validIP)
 	}
 
 	@Validation(id="1079", rtm=["80"])
 	public void authnContextRequired(){
 		def authnContext = header.map.authnStatement.children().findAll{it.name() == 'AuthnContext'}
-		if( authnContext.size() != 1 ) log.error("AuthnContext is required")
 
-		assert(authnContext.size() == 1 )
+		assertTrue("AuthnContext is required", authnContext.size() == 1 )
 	}
 
 	@Validation(id="1080", rtm=["77","85","169"])
 	public void authnContextClassRef() {
 		String authnContextClassRef = header.map.authnStatement.AuthnContext[0].AuthnContextClassRef[0].text().trim()
 
-		if(!(authnContextClassRef in AuthnContextClassRef.values)) log.error("authnContextClassRef not allowed found : {}, but expected one of : {}", authnContextClassRef, AuthnContextClassRef.values)
-		assert (authnContextClassRef in AuthnContextClassRef.values)
+		if(!(authnContextClassRef in AuthnContextClassRef.values)) log.error()
+		assertTrue (MessageFormat.format("authnContextClassRef not allowed found : {0}, but expected one of : {1}", authnContextClassRef, AuthnContextClassRef.values.toString()),  authnContextClassRef in AuthnContextClassRef.values)
 	}
 }
