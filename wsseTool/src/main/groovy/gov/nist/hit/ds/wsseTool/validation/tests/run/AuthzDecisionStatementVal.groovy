cdb9ea5d144f9928b74dd6cbf4fd2782b2355a53
agerardin <none@none>
2013-09-25 17:19:25 -0400

    fitted authzDecisionStatement
    --HG--
    branch : antoine_wsse_update

--- wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AuthzDecisionStatementVal.groovy
+++ wsseTool/src/main/groovy/gov/nist/hit/ds/wsseTool/validation/tests/run/AuthzDecisionStatementVal.groovy
@@ -1,23 +1,22 @@
 package gov.nist.hit.ds.wsseTool.validation.tests.run
 
-import gov.nist.hit.ds.wsseTool.api.config.ValConfig
+import static org.junit.Assert.*
+import gov.nist.hit.ds.wsseTool.api.config.*
 import gov.nist.hit.ds.wsseTool.time.TimeUtil
-import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder;
+import gov.nist.hit.ds.wsseTool.validation.engine.ValRunnerWithOrder
+import gov.nist.hit.ds.wsseTool.validation.engine.annotations.Optional
 import gov.nist.hit.ds.wsseTool.validation.engine.annotations.Validation
 import gov.nist.hit.ds.wsseTool.validation.tests.BaseVal
 import gov.nist.hit.ds.wsseTool.validation.tests.CommonVal
 import gov.nist.toolkit.wsseTool.validation.data.*
 import groovy.util.slurpersupport.GPathResult
 
+import java.text.MessageFormat
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
 import org.joda.time.DateTime
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
-import org.junit.rules.TestName
-import org.junit.runner.RunWith;
+import org.junit.runner.RunWith
 
 
 @RunWith(ValRunnerWithOrder.class)
@@ -29,60 +28,50 @@ public class AuthzDecisionStatementVal extends BaseVal {
 
 			GPathResult children = authz.children()
 
-			if(children[0].name() != "Action") log.error("authz decision statement first child must be an action but was ", children[0])
-			if(children[1].name() != "Evidence") log.error("authz decision statement second child must be an evidence but was ", children[1])
-
-			assert children[0].name() == "Action"
-			assert children[1].name() == "Evidence"
+			assertEquals("authz decision statement first child must be an action","Action", children[0].name())
+			assertEquals("authz decision statement second child must be an evidence", "Evidence", children[1].name())
 		}
 	}
 
 
-	@Validation(id="1091", rtm=["131","133"])
+	@Validation(id="1091", rtm=["131", "133"])
 	public void decision(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
-			if(authz.@Decision != 'Permit') log.error("@Decision must be 'Permit'")
-			assert authz.@Decision == 'Permit'
+			assertEquals("@Decision must be 'Permit'","Permit", authz.@Decision.text())
 		}
 	}
 
-	@Validation(id="1092", rtm=["131","134"] , status=ValConfig.Status.review)
+	@Validation(id="1092", rtm=["131", "134"], status=ValConfig.Status.review)
 	public void resource(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
-			if(authz.@Resource == null) log.error("@Resource must be present")
-			assert authz.@Resource != null
-			
-			String to = context.getParams().get("To");
-			String resource = authz.@Resource;
-			
-			if( resource != "" && resource != to) log.error(" @Resource = <value of S:Header/wsa:To> or ''. Header was {} and expected {} ", resource, to)
+			assertTrue("@Resource must be present", authz.@Resource != null)
+
+			String to = context.getParams().get("To")
+			String resource = authz.@Resource
+
+			assertTrue(MessageFormat.format(" @Resource = <value of S:Header/wsa:To> or ''. Header was {0} and expected {1} ", resource, to), resource == "" || resource == to)
 		}
 	}
 
-	@Validation(id="1093", rtm=["131","132"])
+	@Validation(id="1093", rtm=["131", "132"])
 	public void action(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
-			if(authz.Action[0].text().trim() != "Execute") log.error("action should be : 'Execute' ")
-			assert authz.Action[0].text().trim() == "Execute"
-
-			if(authz.Action[0].@Namespace.text() != "urn:oasis:names:tc:SAML:1.0:action:rwedc") log.error("@Namespace should be : 'urn:oasis:names:tc:SAML:1.0:action:rwedc' ")
-			assert authz.Action[0].@Namespace.text()  == "urn:oasis:names:tc:SAML:1.0:action:rwedc"
+			assertEquals("action should be : 'Execute' ", "Execute", authz.Action[0].text().trim())
+			assertEquals("@Namespace should be : 'urn:oasis:names:tc:SAML:1.0:action:rwedc' ","urn:oasis:names:tc:SAML:1.0:action:rwedc",  authz.Action[0].@Namespace.text())
 		}
 	}
 
 	@Validation(id="1094", rtm=["131"])
 	public void evidence(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
-			if(authz.Evidence[0] == null) log.error("Evidence is required")
-			assert authz.Evidence[0] != null
+			assertTrue("Evidence is required", authz.Evidence[0] != null)
 		}
 	}
 
 	@Validation(id="1095", rtm=["215"])
 	public void containsSamlAssertion(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
-			if(authz.Evidence[0].Assertion[0] == null) log.error("Evidence should contain a saml assertion")
-			assert authz.Evidence[0].Assertion[0] != null
+			assertTrue("Evidence should contain a saml assertion", authz.Evidence[0].Assertion[0] != null)
 		}
 	}
 
@@ -93,9 +82,7 @@ public class AuthzDecisionStatementVal extends BaseVal {
 			Pattern pattern = Pattern.compile('^\\D', Pattern.UNICODE_CASE)
 			Matcher m = pattern.matcher(id)
 
-			if(!m.find()){
-				log.error("ID shall not start with a digit, but {} starts with {}", id , id.charAt(0) )
-			}
+			assertTrue(MessageFormat.format("ID shall not start with a digit, but {0} starts with {0}", id , id.charAt(0)), m.find())
 		}
 	}
 
@@ -104,27 +91,21 @@ public class AuthzDecisionStatementVal extends BaseVal {
 		for(GPathResult authz : header.map.authzDecisionStatements){
 			String d =  authz.Evidence[0].Assertion[0].@IssueInstant.text()
 
-			if(! TimeUtil.isDateInUTCorTimeZoneFormat(d)) log.error("invalid time format for issueInstant : {}", d)
-			assert TimeUtil.isDateInUTCorTimeZoneFormat(d)
+			assertTrue(MessageFormat.format("invalid time format for issueInstant : {0}",d), TimeUtil.isDateInUTCorTimeZoneFormat(d))
 
 			DateTime ii = TimeUtil.parseDateString(d)
 			String creationDate = header.map.timestamp.Created[0].text().trim()
 			DateTime creationTime = TimeUtil.parseDateString(creationDate)
 
-			if(ii.isAfter(creationTime)){
-				log.error("saml2:AuthnStatement@authnInstant {} should be earlier than wsu:Timestamp/wsu:Created {}", ii ,creationTime)
-			}
-
-			assert(!ii.isAfter(creationTime))
+			assertTrue(MessageFormat.format("saml2:AuthnStatement@authnInstant {0} should be earlier than wsu:Timestamp/wsu:Created {1}", ii ,creationTime), !ii.isAfter(creationTime))
 		}
 	}
 
-	@Validation(id="1098", rtm=["66","137"])
+	@Validation(id="1098", rtm=["66", "137"])
 	public void version() {
 		for(GPathResult authz : header.map.authzDecisionStatements){
 			String version =  authz.Evidence[0].Assertion[0].@Version.text()
-			if("2.0" != version) log.error("invalid version : {} expected : 2.0", version)
-			assert "2.0" == version
+			assertEquals("invalid version", "2.0" , version )
 		}
 	}
 
@@ -132,8 +113,7 @@ public class AuthzDecisionStatementVal extends BaseVal {
 	public void issuerIsRequired() {
 		for(GPathResult authz : header.map.authzDecisionStatements){
 			GPathResult issuer =  authz.Evidence[0].Assertion[0].Issuer
-			if(issuer == null) log.error("assertion must have an issuer")
-			assert issuer != null
+			assertTrue("assertion must have an issuer", issuer != null)
 		}
 	}
 
@@ -151,21 +131,17 @@ public class AuthzDecisionStatementVal extends BaseVal {
 
 			if(!notBefore.isEmpty()){
 				DateTime nb = TimeUtil.parseDateString(notBefore)
-				if(nb.isAfter(creationTime)){
-					log.error("@NotBefore {} should be earlier than wsu:Timestamp/wsu:Created {}", nb ,creationTime)
-				}
+				assertTrue(MessageFormat.format("@NotBefore {0} should be earlier than wsu:Timestamp/wsu:Created {1}", nb ,creationTime), !nb.isAfter(creationTime))
 			}
 
 			if(!notOnOrAfter.isEmpty()){
 				DateTime nooa = TimeUtil.parseDateString(notOnOrAfter)
-				if(nooa.isBefore(creationTime)){
-					log.error("@NotOnOrAfter {} should be later than wsu:Timestamp/wsu:Created {}", nooa ,creationTime)
-				}
+				assertTrue(MessageFormat.format("@NotOnOrAfter {0} should be later than wsu:Timestamp/wsu:Created {1}", nooa ,creationTime), !nooa.isBefore(creationTime))
 			}
 		}
 	}
 
-	@Validation(id="1101", rtm=["139","140"])
+	@Validation(id="1101", rtm=["139", "140"])
 	public void attributeStatement(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
 			GPathResult attrStatement =  authz.Evidence[0].Assertion[0].AttributeStatement[0]
@@ -173,13 +149,14 @@ public class AuthzDecisionStatementVal extends BaseVal {
 			GPathResult acp = children.findAll{ it.@Name == "AccessConsentPolicy"}
 			GPathResult iacp = children.findAll{ it.@Name == "InstanceAccessConsentPolicy"}
 
-			if(acp.isEmpty() && iacp.isEmpty() ) log.error(" AttributeStatement MUST contain at least one of the following Attributes:"+
-			"Attribute/@Name = AccessConsentPolicy" +
-			"Attribute/@Name = InstanceAccessConsentPolicy")
+			assertTrue(" AttributeStatement MUST contain at least one of the following Attributes:"+
+					"Attribute/@Name = AccessConsentPolicy" +
+					"Attribute/@Name = InstanceAccessConsentPolicy", !acp.isEmpty() || !iacp.isEmpty() )
 		}
 	}
 
-	@Validation(id="1102", rtm=["140"], status=ValConfig.Status.review, category="optional")
+	@Optional
+	@Validation(id="1102", rtm=["140"], status=ValConfig.Status.review)
 	public void AccessConsentPolicy(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
 			GPathResult attrStatement =  authz.Evidence[0].Assertion[0].AttributeStatement[0]
@@ -190,21 +167,18 @@ public class AuthzDecisionStatementVal extends BaseVal {
 				log.info("AccessConsentPolicy not present") ; return
 			}
 
-			if(acp[0].@NameFormat != "http://www.hhs.gov/healthit/nhin")
-				log.error("@nameFormat must be {}, found {}", "http://www.hhs.gov/healthit/nhin", acp[0].@NameFormat )
-			
-			if(acp[0].children().isEmpty())
-				log.error("accessConsentPolicy must have at least one attributeValue child element")
-			
+			assertEquals("@nameFormat must be http://www.hhs.gov/healthit/nhin","http://www.hhs.gov/healthit/nhin", acp[0].@NameFormat.text())
+
+			assertTrue("accessConsentPolicy must have at least one attributeValue child element",!acp[0].children().isEmpty())
+
 			for(GPathResult attr : acp[0].children()){
 				boolean isValid = CommonVal.validURN( attr.text().trim() )
-				
-				if(!isValid)
-					log.error("attribute value must be a valid urn. Found : {}", attr.text().trim() );
-			}			
+
+				assertTrue(MessageFormat.format("attribute value must be a valid urn. Found : {0}", attr.text().trim() ), isValid)
+			}
 		}
 	}
-	
+
 	@Validation(id="1103", rtm=["140"], category="optional")
 	public void InstanceAccessConsentPolicy(){
 		for(GPathResult authz : header.map.authzDecisionStatements){
@@ -216,19 +190,14 @@ public class AuthzDecisionStatementVal extends BaseVal {
 				log.info("InstanceAccessConsentPolicy not present") ; return
 			}
 
-			if(iacp[0].@NameFormat != "http://www.hhs.gov/healthit/nhin")
-				log.error("@nameFormat must be {}, found {}", "http://www.hhs.gov/healthit/nhin", iacp[0].@NameFormat )
-			
-			if(iacp[0].children().isEmpty()) 
-				log.error("instanceAccessConsentPolicy must have at least one attributeValue child element")
-			
+			assertEquals("@nameFormat must be http://www.hhs.gov/healthit/nhin", "http://www.hhs.gov/healthit/nhin", iacp[0].@NameFormat.text())
+
+			assertFalse("instanceAccessConsentPolicy must have at least one attributeValue child element", iacp[0].children().isEmpty())
+
 			for(GPathResult attr : iacp[0].children()){
 				boolean isValid = CommonVal.validURN( attr.text().trim() )
-				
-				if(!isValid)
-					log.error("attribute value must be a valid urn. Found : {}", attr.text().trim() );
+				assertTrue(MessageFormat.format("attribute value must be a valid urn. Found : {0}", attr.text().trim()), isValid )
 			}
 		}
 	}
-
 }
