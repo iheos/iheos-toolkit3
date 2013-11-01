f9c7c161e75b5696cbd2f72ed688cecfb780365d
Sunil Bhaskarla <skbhaskarla@gmail.com>
2013-11-01 15:04:38 -0400

    Amend previous cc: refactored Modified property to Updated property and introduce new Modified date property.
    --HG--
    branch : feature/repository

--- repository/src/main/java/gov/nist/hit/ds/repository/api/PropertyKey.java
+++ repository/src/main/java/gov/nist/hit/ds/repository/api/PropertyKey.java
@@ -59,11 +59,11 @@ public enum PropertyKey {
                return "mimeType";
            }
        },
-       UPDATED_DATE() {
-       	@Override
-           public String toString() {
-               return "modifiedDate";
-           }
+       MODIFIED_DATE() {
+          	@Override
+              public String toString() {
+                  return "modifiedDate";
+              }
        },
        PARENT_ID(){
            @Override
@@ -82,6 +82,12 @@ public enum PropertyKey {
            public String toString() {
                return "status";
            }
-       };
+       },
+       UPDATED_DATE() {
+          	@Override
+            public String toString() {
+                return "updatedDate";
+            }
+        };
 
    }
