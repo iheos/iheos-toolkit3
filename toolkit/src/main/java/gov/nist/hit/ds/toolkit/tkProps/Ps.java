16388d32c065d3157a646083df162c2f0dfc9454
bmajur <none@none>
2014-06-03 17:16:15 -0400

    Updates to toolkit
    --HG--
    branch : feature/simulator

---
+++ toolkit/src/main/java/gov/nist/hit/ds/toolkit/tkProps/Ps.java
@@ -0,0 +1,11 @@
+package gov.nist.hit.ds.toolkit.tkProps;
+
+public class Ps {
+	public String name;
+	public String value;
+	
+	public Ps(String name, String value) {
+		this.name = name;
+		this.value = value;
+	}
+}
