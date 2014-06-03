b0403cd3b0e34b63f8e6f7371dcaf0140c5c0dc9
bmajur <none@none>
2014-06-03 17:21:36 -0400

    Updates to event-log
    --HG--
    branch : feature/simulator

---
+++ event-log/src/main/groovy/gov/nist/hit/ds/eventLog/Artifacts.groovy
@@ -0,0 +1,12 @@
+package gov.nist.hit.ds.eventLog;
+
+public class Artifacts {
+    def artifactMap = [:]
+    def artifactOrder = []
+
+    def add(name, value) {
+        artifactMap[name] = value
+        artifactOrder << name
+    }
+
+}
