e3c22b5c06920a31e19dcd5eccd3b7879c1d131c
dazais <none@none>
2014-06-10 13:23:59 -0400

    cleaning up poms
    --HG--
    branch : feature/gui/merge

--- xdstools3/src/main/java/gov/nist/toolkit/xdstools3/client/restDatasourceTest/message/Message.java
+++
@@ -1,9 +0,0 @@
-package gov.nist.toolkit.xdstools3.client.restDatasourceTest.message;
-
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
-public class Message {
-    private String value;
-
-}
