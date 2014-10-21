411e1def023a5c7c51091c547fe396eedfd73931
dazais <none@none>
2014-10-21 17:35:16 -0400

    Commented out deprecated code in Popup message class.
    --HG--
    branch : feature/gui

--- xdstools2_v2/src/main/java/gov/nist/toolkit/xdstools2/client/adapter2v3/PopupMessageV3.java
+++ xdstools2_v2/src/main/java/gov/nist/toolkit/xdstools2/client/adapter2v3/PopupMessageV3.java
@@ -4,15 +4,11 @@ import com.smartgwt.client.types.Alignment;
 import com.smartgwt.client.util.EventHandler;
 import com.smartgwt.client.widgets.Button;
 import com.smartgwt.client.widgets.Dialog;
-import com.smartgwt.client.widgets.HTMLFlow;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
 import com.smartgwt.client.widgets.events.KeyDownEvent;
 import com.smartgwt.client.widgets.events.KeyDownHandler;
-import com.smartgwt.client.widgets.layout.VStack;
-import gov.nist.toolkit.results.client.AssertionResult;
-import gov.nist.toolkit.results.client.AssertionResults;
 
 public class PopupMessageV3 extends Dialog {
 
@@ -23,8 +19,7 @@ public class PopupMessageV3 extends Dialog {
         Label lab =  new Label(message);
         addItem(lab);
         lab.setLayoutAlign(Alignment.CENTER);
-        lab.setAutoFit(true);
-        setWidth(350);
+        //lab.setAutoFit(true);
         lab.setWidth(300);
 
         // OK button
@@ -48,41 +43,42 @@ public class PopupMessageV3 extends Dialog {
 		addItem(ok);
 		ok.setLayoutAlign(Alignment.CENTER);
 
-        // popup attributes
-        setTitle("Error");
-        setPadding(5);
-        setPaddingAsLayoutMargin(true);
-        draw();
-
         // Set the focus on the button (has to be set only after drawing the widget)
         ok.focus();
+
+        // popup attributes
+        setTitle("Error");
+        setAutoSize(true);
+        setMinWidth(350);
+
+        draw();
 	}
 
-	public PopupMessageV3(AssertionResults result) {
-        super();
 
-        VStack panel = new VStack();
-		addItem(panel);
-		for (AssertionResult ar : result.assertions) {
-			if (ar.status) {
-                panel.addMember(new HTMLFlow(ar.assertion));
-			} else {
-				panel.addMember(new HTMLFlow("<font color=\"#FF0000\">" + ar.assertion + "</font>"));
-			}
-		}
-		// DialogBox is a SimplePanel, so you have to set its widget property to
-		// whatever you want its contents to be.
-		Button ok = new Button("OK");
-		ok.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				removeFromParent();
-			}
-		});
-		panel.addMember(ok);
-        setAlign(Alignment.CENTER);
-
-		draw();
-	}
+// TODO this is legacy code, not updated yet
+//	public PopupMessageV3(AssertionResults result) {
+//        super();
+//
+//        VStack panel = new VStack();
+//		addItem(panel);
+//		for (AssertionResult ar : result.assertions) {
+//			if (ar.status) {
+//                panel.addMember(new HTMLFlow(ar.assertion));
+//			} else {
+//				panel.addMember(new HTMLFlow("<font color=\"#FF0000\">" + ar.assertion + "</font>"));
+//			}
+//		}
+//
+//		Button ok = new Button("OK");
+//		ok.addClickHandler(new ClickHandler() {
+//			public void onClick(ClickEvent event) {
+//				removeFromParent();
+//			}
+//		});
+//		panel.addMember(ok);
+//        setAlign(Alignment.CENTER);
+//		draw();
+//	}
 
 }
 
