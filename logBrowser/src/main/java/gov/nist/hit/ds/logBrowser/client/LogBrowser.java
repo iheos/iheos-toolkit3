4fbc13a54e277c2422bb97ab6c839d170365c04e
Sunil Bhaskarla <skbhaskarla@gmail.com>
2015-01-08 17:26:42 -0500

    Enable log browser features (affects standalone log browser web app only)
    --HG--
    branch : feature/repository/generalupdates

--- logBrowser/src/main/java/gov/nist/hit/ds/logBrowser/client/LogBrowser.java
+++ logBrowser/src/main/java/gov/nist/hit/ds/logBrowser/client/LogBrowser.java
@@ -44,12 +44,12 @@ public class LogBrowser implements EntryPoint {
               } else {
                   logBrowserWidget = new LogBrowserWidget(eventBus,
                           new Feature[] {
-//                                  Feature.BROWSE
-//                                  , Feature.SEARCH
+                                  Feature.BROWSE
+                                  , Feature.SEARCH
 //                                , Feature.TRANSACTION_MONITOR // Old
 //                                , Feature.TRANSACTION_FILTER // Old
-//                                , Feature.TRANSACTION_FILTER_ADVANCED
-                                 Feature.EVENT_MESSAGE_AGGREGATOR // Independent widget test area
+                                , Feature.TRANSACTION_FILTER_ADVANCED
+//                                , Feature.EVENT_MESSAGE_AGGREGATOR // Independent widget test area
 //                                 , Feature.LOGGING_CONTROL  // Experimental, comment off all others above
                           });
 
